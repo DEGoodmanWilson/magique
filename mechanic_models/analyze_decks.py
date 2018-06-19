465396f93228b8bb2aa326dad7853067066bf990
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-06-19 07:04:02 +0200

    Be smarter about computing conditional probs

--- mechanic_models/analyze_decks.py
+++ mechanic_models/analyze_decks.py
@@ -24,22 +24,37 @@ with open('../data/card_tags.json') as f:
     card_tags = json.load(f)
 
 for deck in decks:
-    deck = deck.replace('"', '')
-    deck = deck.replace('\n', '')
-    deck = re.split("\s?(\d)\s", deck)[1:]
-    deck = zip(deck[0::2], deck[1::2])
+    # Process this non-format into something we can use. Each deck looks like:
+    #"2 Card of Death 3 Scrape 2 Another Card 1 Singleton\n"
+    # _with_ the double quotes, ugh
+    deck = deck.replace('"', '') # remove double quotes
+    deck = deck.replace('\n', '') # remove the newline
+
+    deck = re.split("\s?\d\s", deck)[1:] # parse deck out into "Card of Death", "Scrape", "Another Card",...
+
+    # deck = re.split("\s?(\d)\s", deck)[1:] # parse deck out into "2", "Card of Death", "3", "Scrape", ...
+    # deck = zip(deck[0::2], deck[1::2])
     # we now have a deck that looks like
     # [ ("1", "Abrade"), ("4", "Cancel"), ...]
-    # Let's turn this into a list of cards, then a list of abilities.
+
+    # Let's turn this into a list of cards with quantities, then a list of abilities.
+    # deck_mechanics = []
+    # for card in deck:
+    #     name = card[1]
+    #     count = int(card[0])
+    #     for i in range(count):
+    #         card_tagged = name in card_tags
+    #         if(card_tagged):
+    #             for tag in card_tags[name]:
+    #                 deck_mechanics.append(tag)
+
+    # Let's turn this into a list of abilities.
     deck_mechanics = []
-    for card in deck:
-        name = card[1]
-        count = int(card[0])
-        for i in range(count):
-            card_tagged = name in card_tags
-            if(card_tagged):
-                for tag in card_tags[name]:
-                    deck_mechanics.append(tag)
+    for card_name in deck:
+        card_tagged = card_name in card_tags
+        if(card_tagged):
+            for tag in card_tags[card_name]:
+                deck_mechanics.append(tag)
     
     deck_mechanics.sort()
     deck_mechanics_set = set(deck_mechanics) # Get a list of unique mechanics
@@ -47,8 +62,13 @@ for deck in decks:
         mechanic_appearances[mechanic] += 1
 
     for mechanic1 in deck_mechanics_set:
-        for mechanic2 in deck_mechanics: # We are not iterating over unique items here. This lets mechanics that appear more frequently be more represented
-            mechanic_combo_appearances[mechanic1][mechanic2] += 1
+        # First, let's identify when two distinct mechanics co-occur
+        for mechanic2 in deck_mechanics_set:
+            if mechanic1 != mechanic2:
+                mechanic_combo_appearances[mechanic1][mechanic2] += 1
+        # Now let's identify if the same mechanic co-occurs on different cards
+        if deck_mechanics.count(mechanic1) > 1:
+            mechanic_combo_appearances[mechanic1][mechanic1] += 1
 
 
 conditional_probabilities = {}
