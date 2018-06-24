31214e63aac4e82679a96246414cd700b67e1f92
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-06-24 17:23:12 +0200

    Now we can extract the types of tokens a card creates, and add that to the card's subtypes. C++ GA code doesn't support this behavior yet.

--- mechanic_models/card_affinities.py
+++ mechanic_models/card_affinities.py
@@ -6,8 +6,7 @@ import re
 with open('../data/AllCards-x.json') as json_data:
     cards = json.load(json_data)
 
-all_card_affinities = {}
-
+all_card_affinities_and_subtypes = {}
 
 with open('../data/types.json') as json_types:
     types = json.load(json_types)
@@ -17,7 +16,7 @@ with open('../data/types.json') as json_types:
 # Sarpadian Empires, Vol. VII
 # Also "Angrath's Fury", ugh.
 
-token_re = re.compile(r"([A-Z]\w* )+(artifact )?creature token")
+token_re = re.compile(r"((?:[A-Z]\w* )+)(?:artifact )?creature token")
 
 type_res = []
 for type in types:
@@ -33,18 +32,30 @@ for name, card in cards.iteritems():
         continue
 
     if 'text' in card:
-        card_affinities = []
-
+        card_annotations = {}
         # remove any reference to the cards name, like "Aspect of Mongoose" or "Fire Dragon"
         text = card['text'].replace(card['name'], '')
 
-        text = re.sub(token_re, 'creature token', text)
+        # remove any reference to token creation; creating a token does not indiciate an affinity for that card type
+        no_token_text = re.sub(token_re, 'creature token', text)
 
-        # check for references to creature types.
+        # check for references to creature types to find creature types this card has an affinity for.
+        card_affinities = []
         for i in range(len(types)):
-            if(re.search(type_res[i], text)):
+            if(re.search(type_res[i], no_token_text)):
                 card_affinities.append(types[i])
         if (len(card_affinities) > 0):
-            all_card_affinities[name] = card_affinities
+            card_annotations['affinities'] = card_affinities
 
-print(json.dumps(all_card_affinities, indent=4, separators=(',', ': ')))
+        # Now, if this is a creature card, and it creates a token, add that token type to this creatures subtypes
+        card_additional_subtypes = []
+        token_matches = re.search(token_re, text)
+        if(token_matches != None):
+            new_subtypes = token_matches.group(1)
+            card_annotations['sub_types'] = new_subtypes.split()
+
+        # Add them into the global object
+        if(card_annotations):
+            all_card_affinities_and_subtypes[name] = card_annotations
+
+print(json.dumps(all_card_affinities_and_subtypes, indent=4, separators=(',', ': ')))
