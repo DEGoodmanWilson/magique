3e48fd64385de97a1236e3d62d5130c831a3c4d3
Don Goodman-Wilson <degoodmanwilson@github.com>
2019-02-26 08:14:20 +0000

    Mkdirs as we go

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ popd
 
 # Now let's grab top decks for each format for analysis, then analyse them for synergies!
 pushd synergies
+mkdir ../../data/synergies
 echo "Finding synergies..."
 ./run.sh
 echo "...Done"
@@ -20,6 +21,7 @@ popd
 
 # Let's grab data from EDHRC (Warning, will take a long time!)
 pushd edhrec
+mkdir ../../data/edhrec
 echo "Downloading data from EDHREC **WARNING WILL TAKE A WHILE**..."
 ./run.sh
 echo "...Done"
